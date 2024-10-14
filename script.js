20baad50b46461a9fac9f09388cd2444781f9ab3
homeylau <lhw1953927853@gmail.com>
2024-10-14 11:19:19 +0800

    add complete logics and adjust the UI again

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-let humanScore = 0;
-let computerScore = 0;
-
 function getComputerChoice() {
   let choice = String(Math.floor(Math.random() * 3));
   let result = "";
@@ -18,61 +15,91 @@ function getComputerChoice() {
   return result;
 }
 
-function getHumanChoice() {
-  let input = prompt(
-    "Enter you choice(rock paper scissors): ",
-    getComputerChoice()
-  );
-  input = input.toLowerCase();
-  input = input.trim();
-  switch (input) {
-    case "rock":
-      break;
-    case "paper":
-      break;
-    case "scissors":
-      break;
-    default:
-      console.error("Invalid input!");
-  }
-  return input;
+function resetGameState() {
+  humanScore = 0;
+  computerScore = 0;
+  updateGameScore();
+  const gameLog = document.querySelectorAll("#game-progress li");
+  gameLog.forEach((node) => {
+    node.remove();
+  });
 }
 
+function updateGameScore() {
+  const humanPoints = document.querySelector(
+    "#points-container > .human-score"
+  );
+  humanPoints.textContent = `HUMAN SCORE: ${humanScore}`;
+  const computerPoints = document.querySelector(
+    "#points-container > .computer-score"
+  );
+  computerPoints.textContent = `COMPUTER SCORE: ${computerScore}`;
+}
+
+function addGameLog(text) {
+  const res = document.createElement("li");
+  res.textContent = text;
+  result.appendChild(res);
+}
+
+let humanScore = 0;
+let computerScore = 0;
+const result = document.querySelector("#game-progress");
+
 function playRound(humanChoice, computerChoice) {
+  addGameLog(`You chose ${humanChoice} While Computer chose ${computerChoice}`);
+
   if (humanChoice == computerChoice) {
-    console.log("tie!");
+    addGameLog("Tie!");
   } else {
     if (
       (humanChoice === "rock" && computerChoice === "scissors") ||
       (humanChoice === "paper" && computerChoice === "rock") ||
       (humanChoice === "scissors" && computerChoice === "paper")
     ) {
-      console.log("Human Win!");
+      addGameLog("Human Win This Round!");
       humanScore++;
     } else {
-      console.log("Computer Win!");
+      addGameLog("Computer Win This Round!");
       computerScore++;
     }
   }
-
-  console.log(
-    `Now human score is ${humanScore} VS computer score ${computerScore}`
-  );
+  updateGameScore();
+  if (humanScore >= 3) {
+    alert("You Win!");
+    resetGameState();
+  } else if (computerScore >= 3) {
+    alert("Computer Win!");
+    resetGameState();
+  }
   return;
 }
 
-for (let i = 0; i < 5; i++) {
-  const humanSelection = getHumanChoice();
-  const computerSelection = getComputerChoice();
-  console.log(`You choose ${humanSelection}`);
-  console.log(`Computer choose ${computerSelection}`);
-  playRound(humanSelection, computerSelection);
+function getHumanClickedButton(e) {
+  let humanChoice = "invalid";
+  switch (e.target.className) {
+    case "rock":
+      humanChoice = "rock";
+      break;
+    case "paper":
+      humanChoice = "paper";
+      break;
+    case "scissors":
+      humanChoice = "scissors";
+      break;
+    case "reset":
+      resetGameState();
+      return;
+    default:
+      return;
+  }
+  if (humanChoice !== "invalid") {
+    const humanSelection = humanChoice;
+    const computerSelection = getComputerChoice();
+    playRound(humanSelection, computerSelection);
+  }
 }
 
-if (humanScore > computerScore) {
-  alert("Finally human win!");
-} else if (humanScore < computerScore) {
-  alert("Finally computer win!");
-} else {
-  alert("Tie!");
-}
+updateGameScore();
+const choice = document.querySelector("#choice-section");
+choice.addEventListener("click", getHumanClickedButton);
